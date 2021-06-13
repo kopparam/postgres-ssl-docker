8828ebd2f526e5034e212bdb65c8eda3952b4907
Kashyap Kopparam <kashyapkopparam@gmail.com>
2021-06-13 18:17:14 +0800

    make create script idempotent

---
+++ README.md
@@ -0,0 +1,7 @@
+# Run PostgreSQL with SSL locally using Docker
+
+## Prerequisites
+1. Docker
+
+## Setup
+1. Run `docker-compose up`
--- create-certs.sh
+++ create-certs.sh
@@ -5,19 +5,43 @@ set -o nounset \
     -o verbose \
     -o xtrace
 
-openssl req -new -x509 -days 3650 -nodes -text -out server-cert.pem \
-  -keyout server-key.pem -subj "/CN=localhost"
+if [ ! -d certs ]; then
+  mkdir certs
+fi
 
-chmod og-rwx server-key.pem
+pushd certs
 
-openssl req -new -nodes -text -out root.csr \
-  -keyout root-key.pem -subj "/CN=localhost"
-chmod og-rwx root-key.pem
+  echo "Creating server certificates"
+  if [ ! -e server-key.pem ]; then
+    openssl req -new -x509 -days 3650 -nodes -text -out server.crt \
+      -keyout server-key.pem -subj "/CN=localhost"
+    chmod og-rwx server-key.pem
+  fi
 
-openssl req -new -nodes -text -out server.csr \
-  -keyout server-key-signed.pem -subj "/CN=localhost"
-chmod og-rwx server-signed.key
+  if [ ! -e root.csr & ! -e root-key.pem ]; then
+    echo "Creating root key and signing request and root certificate"
+    openssl req -new -nodes -text -out root.csr \
+      -keyout root-key.pem -subj "/CN=localhost"
+    chmod og-rwx root-key.pem
+  fi
 
-openssl x509 -req -in server.csr -text -days 3650 \
-  -CA root-cert.pem -CAkey root-key.pem -CAcreateserial \
-  -out server-cert-signed.pem
+  if [ ! -e root.crt ]; then
+    openssl x509 -req -in root.csr -text -days 3650 \
+    -extfile /etc/ssl/openssl.cnf -extensions v3_ca \
+    -signkey root-key.pem -out root.crt
+  fi
+
+  if [ ! -e server-key-signed.pem ]; then
+    echo "Signing server key and certificate with CA"
+    openssl req -new -nodes -text -out server.csr \
+      -keyout server-key-signed.pem -subj "/CN=localhost"
+    chmod og-rwx server-key-signed.pem
+  fi
+
+  if [ ! -e server-signed.crt ]; then
+    openssl x509 -req -in server.csr -text -days 3650 \
+      -CA root.crt -CAkey root-key.pem -CAcreateserial \
+      -out server-signed.crt
+  fi
+
+popd
